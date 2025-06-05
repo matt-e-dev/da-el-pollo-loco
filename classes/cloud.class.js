107f5d8dbce341934f8fd2126e8d29781ace0c76
matt-e-dev <dmdmlbwski@gmail.com>
2025-06-05 11:25:21 +0200

    animation added for clouds

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -8,8 +8,14 @@ class Cloud extends MoveableObject {
   constructor() {
     super();
     this.loadImage("img/5_background/layers/4_clouds/1.png");
-
+    this.animate();
   }
 
+  animate() {
+
+    setInterval(() => {
+      this.x -= 0.15;
+    }, 1000 / 60)
+  }
 }
 
