a09a8e88287a8914285d382d09f18f28f2245cb8
matt-e-dev <dmdmlbwski@gmail.com>
2025-06-26 14:54:40 +0200

    character cant leave map to the right added

--- classes/character.class.js
+++ classes/character.class.js
@@ -24,7 +24,7 @@ class Character extends MoveableObject {
 
   animate() {
     setInterval(() => {
-        if (this.world.keyboard.RIGHT) {
+        if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
           this.x += this.speed;
           this.otherDirection = false;
             console.log("Moving RIGHT");
--- classes/level.class.js
+++ classes/level.class.js
@@ -3,6 +3,7 @@ class Level {
     enemies;
     clouds;
     backgroundObjects;
+    level_end_x = 1400;
 
     constructor(enemies, clouds, backgroundObjects) {
         this.enemies = enemies;
