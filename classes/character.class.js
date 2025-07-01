207f1ae8a0273731501dfc0428acffb6a594d716
matt-e-dev <dmdmlbwski@gmail.com>
2025-07-01 17:26:31 +0200

    jumping animation added

--- classes/character.class.js
+++ classes/character.class.js
@@ -13,6 +13,20 @@ class Character extends MoveableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
 
+  IMAGES_JUMPING = [
+
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-33.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png",
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
+
+  ]
+
   currentImage = 0;
   world;
 
@@ -20,6 +34,7 @@ class Character extends MoveableObject {
     super();
     this.loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.animate();
     this.applyGravity();
   }
@@ -29,21 +44,24 @@ class Character extends MoveableObject {
         if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
           this.x += this.speed;
           this.otherDirection = false;
-            console.log("Moving RIGHT");
+            
         }
         if (this.world.keyboard.LEFT && this.x > 0) {
             this.x -= this.speed;
-          console.log("Moving LEFT");
           this.otherDirection = true;
         }
       this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
 
     setInterval(() => {
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING);
+      } else {
         if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-            // Walk animation
+          // Walk animation
           this.playAnimation(this.IMAGES_WALKING);
         }
+      }
     }, 50);
   }
 }
