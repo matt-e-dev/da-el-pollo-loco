331d60d4cc8e43a98cc7ea70f2fc683e49fc5029
matt-e-dev <dmdmlbwski@gmail.com>
2025-08-21 08:45:30 +0200

    small chicken enemies added

---
+++ classes/chicken-small.class.js
@@ -0,0 +1,42 @@
+class SmallChicken extends MoveableObject {
+
+    height = 50;
+    width = 50;
+    IMAGES_WALKING = [
+        "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+        "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+        "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+    ];
+
+  constructor() {
+    super();
+    this.loadImage("img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
+
+    this.moveLeft();
+    this.speed = 0.15 * Math.random() * 5;
+    this.x = 200 + Math.random() * 500; // Random x position
+    this.y = 400;
+    this.animate();
+      this.loadImages(this.IMAGES_WALKING);
+    
+      
+   
+  }
+
+  animate() {
+    setInterval(() => {
+          this.moveLeft();
+          if (Math.random() < 0.2) {
+            this.jump();
+          }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_WALKING);
+    }, 100);
+  }
+    
+  
+}
+
+
--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,5 @@
 const level1 = new Level(
-    [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
+    [new Chicken(), new Chicken(), new Chicken(), new SmallChicken(), new SmallChicken(), new SmallChicken(), new Endboss()],
     [new Cloud(), new Cloud()],
     [
         new BackgroundObject("img/5_background/layers/air.png", -719),
